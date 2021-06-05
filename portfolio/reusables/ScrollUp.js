ea9c1f71819e420cdb22b437cb2770300add996b
ericus123 <amaniericus@gmail.com>
2021-06-05 18:59:11 +0200

    feat(landing-page):add landing page

---
+++ portfolio/reusables/ScrollUp.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { ArrowUpShort } from "react-bootstrap-icons";
+
+const ScrollButton = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const scrolled = process.browser
+      ? document.documentElement.scrollTop
+      : null;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    process.browser
+      ? window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        })
+      : null;
+  };
+
+  process.browser ? window.addEventListener("scroll", toggleVisible) : null;
+
+  return (
+    <div
+      className="scroll-to-top rounded"
+      style={{ display: visible ? "inline" : "none" }}
+      onClick={scrollToTop}
+    >
+      <ArrowUpShort />
+    </div>
+  );
+};
+
+export default ScrollButton;
