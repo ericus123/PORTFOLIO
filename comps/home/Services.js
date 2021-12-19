7103e9cfe4f74eed00ae15c3207d87a53fcfc6cf
AMANI <amaniericus@gmail.com>
2021-12-19 05:25:15 -0500

    ft(services):fix services design

--- comps/home/Services.js
+++ comps/home/Services.js
@@ -10,102 +10,99 @@ import styles from "./index.module.scss";
 
 const Services = () => {
   return (
-    <>
-      <div className=" section container-fluid mb-5" id="services">
-        <br />
-        <h2 className="section-title">Services</h2>
+    <div className="services__container" id="services">
+      <br />
+      <h2 className="section-title">Services</h2>
 
-        <div className="row">
-          <div className="col-md-4">
-            <div className={styles.box}>
-              <div className={`${styles.our_services} ${styles.settings}`}>
-                <div className={styles.icon}>
-                  <Image src={ui_ux} priority quality={25} />
-                </div>
-                <h6>UI/UX DESIGN</h6>
-                <p>
-                  Translating requirements into style design patterns and create
-                  user flows, wireframes, prototypes and mockups.
-                </p>
+      <div className="row">
+        <div className="col-md-6 col-lg-4">
+          <div className={styles.box}>
+            <div className={`${styles.our_services} ${styles.settings}`}>
+              <div className={styles.icon}>
+                <Image src={ui_ux} priority quality={25} />
               </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className={styles.box}>
-              <div className={`${styles.our_services} ${styles.speedup}`}>
-                <div className={styles.icon}>
-                  <Image src={optimization} priority quality={25} />
-                </div>
-                <h6>SEO</h6>
-                <p>
-                  Monitoring search algorithms set by search engine and
-                  developing and integrating content marketing strategies.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className={styles.box}>
-              <div className={`${styles.our_services} ${styles.privacy}`}>
-                <div className={styles.icon}>
-                  <Image src={backend} priority quality={25} />
-                </div>
-                <h6>BACKEND DEVELOPMENT</h6>
-                <p>
-                  Building and maintaining web applications,boosting current
-                  applications and managing hosting environments.
-                </p>
-              </div>
+              <h6>UI/UX DESIGN</h6>
+              <p>
+                Translating requirements into style design patterns and create
+                user flows, wireframes, prototypes and mockups.
+              </p>
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-4">
-            <div className={styles.box}>
-              <div className={`${styles.our_services} ${styles.backups}`}>
-                <div className={styles.icon}>
-                  <Image src={responsive} priority quality={25} />
-                </div>
-                <h5>RESPONSIVE WEB DESIGN</h5>
-                <p>
-                  create content that adjusts smoothly to various screen sizes
-                  to automatically adapt to the browser space.
-                </p>
+        <div className="col-md-6 col-lg-4">
+          <div className={styles.box}>
+            <div className={`${styles.our_services} ${styles.speedup}`}>
+              <div className={styles.icon}>
+                <Image src={optimization} priority quality={25} />
               </div>
+              <h6>SEO</h6>
+              <p>
+                Monitoring search algorithms set by search engine and developing
+                and integrating content marketing strategies.
+              </p>
             </div>
           </div>
-          <div className="col-md-4">
-            <div className={styles.box}>
-              <div className={`${styles.our_services} ${styles.ssl}`}>
-                <div className={styles.icon}>
-                  <Image src={database} />
-                </div>
-                <h6>DATABASE MANAGEMENT</h6>
-                <p>
-                  Develop and maintain databases. Create data storage and
-                  retrieval systems, troubleshoot database issues.
-                </p>
+        </div>
+        <div className="col-md-6 col-lg-4">
+          <div className={styles.box}>
+            <div className={`${styles.our_services} ${styles.privacy}`}>
+              <div className={styles.icon}>
+                <Image src={backend} priority quality={25} />
               </div>
+              <h6>BACKEND DEVELOPMENT</h6>
+              <p>
+                Building and maintaining web applications,boosting current
+                applications and managing hosting environments.
+              </p>
             </div>
           </div>
-          <div className="col-md-4">
-            <div className={styles.box}>
-              <div className={`${styles.our_services} ${styles.database}`}>
-                <div className={styles.icon}>
-                  <Image src={frontend} priority quality={25} />
-                </div>
-                <h6>FRONTEND DEVELOPMENT</h6>
-                <p>
-                  {" "}
-                  Implementing visual elements and combining the art of design
-                  with the science of programming.
-                </p>
+        </div>
+
+        <div className="col-md-6 col-lg-4">
+          <div className={styles.box}>
+            <div className={`${styles.our_services} ${styles.backups}`}>
+              <div className={styles.icon}>
+                <Image src={responsive} priority quality={25} />
               </div>
+              <h6>RESPONSIVE WEB DESIGN</h6>
+              <p>
+                create content that adjusts smoothly to various screen sizes to
+                automatically adapt to the browser space.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6 col-lg-4">
+          <div className={styles.box}>
+            <div className={`${styles.our_services} ${styles.ssl}`}>
+              <div className={styles.icon}>
+                <Image src={database} />
+              </div>
+              <h6>DATABASE MANAGEMENT</h6>
+              <p>
+                Develop and maintain databases. Create data storage and
+                retrieval systems, troubleshoot database issues.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6 col-lg-4">
+          <div className={styles.box}>
+            <div className={`${styles.our_services} ${styles.database}`}>
+              <div className={styles.icon}>
+                <Image src={frontend} priority quality={25} />
+              </div>
+              <h6>FRONTEND DEVELOPMENT</h6>
+              <p>
+                {" "}
+                Implementing visual elements and combining the art of design
+                with the science of programming.
+              </p>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
