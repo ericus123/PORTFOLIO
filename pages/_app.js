68839fb7142bed75e640d74ad45aed4cc02ef7a4
AMANI <amaniericus@gmail.com>
2021-12-20 06:15:34 -0500

    fix(ads):fix google ads

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,7 @@ function MyApp({ Component, pageProps }) {
         (adsbygoogle = window.adsbygoogle || []).push({});
       } catch (e) {}
     }
-  }, [router]);
+  }, [router.asPath]);
   return (
     <>
       <Head>
